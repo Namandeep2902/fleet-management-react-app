7e41128c497d28cf6c2a379de6f4705ae59bfde5
Namandeep2902 <namandeepjouhal@gmail.com>
2025-12-28 20:08:15 +0530

    Commit 2

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,31 @@
+import {useState, useRef, useEffect} from "react";
+import{useNavigate} from "react-router-dom";
+const Login = () => {
+    const [email, setaemail] = useState("");
+    const [password, setPassword] = useState("");
+    constemailRef = useRef();
+    const navigate = useNavigate();
+    useEffect(() => {
+        emailRef.current.focus();
+    },[]);
+    const handleLogin = () =>
+    {
+        if(email === "admin@gmail.com" && password === "admin1234")
+        {
+            alert("Login Success");
+            localStorage.setItem("auth",true);
+            navigate("/admin");
+        }
+        else {
+            alert("Wrong Email or Password");
+        }
+    };
+    return(
+        <>
+        <input ref ={emailRef} value ={email} onChange={(e) => setemail(e.target.value)} />
+        <input type="password" value ={password} onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={handleLogin}>Login</button>
+        </>
+    );
+    };
+export default Login;
